62a59712f895a994b255193ee7cd3b6391e3e386
Jake Peterson <jpeterson0514@gmail.com>
2020-09-09 22:06:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,7 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_results = data.each_with_object ({}) do 
+    |(key, value)|, final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if final_array
 end

3a13332b822c3d30856dae78e1e8d30caef20fdc
Jake Peterson <jpeterson0514@gmail.com>
2020-09-09 22:09:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,14 @@ def nyc_pigeon_organizer(data)
     |(key, value)|, final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        if final_array
+        if final_array[name]
+          final_array[name] = {}
+        end 
+        if final_array[name][key]
+          final_array[name][key] = []
+        end 
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
+  end
 end
